35f2dc29c45b82689027fe1d38605efcd5ca886f
L-cir94 <luca.cirigliano94@hotmail.it>
2023-05-17 14:52:04 +0200

    function for take genre

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 class Movie
 {
-//composition
+    //composition
     public $name;
     /* public $genre; */
     public $imgPath;
     public $year;
     public $rate;
 
-    public function __construct(string $name, /* string $genre ,*/ Genre $genre, int $year,string $imgPath, string $rate)
+    public function __construct(string $name, /* string $genre ,*/ Genre $genre, int $year, string $imgPath, string $rate)
     {
         $this->name = $name;
         $this->genre = $genre;
@@ -18,10 +18,10 @@ class Movie
         $this->imgPath = $imgPath;
     }
     //method
-    public function getMovieRateyear()
+    public function getGenre()
     {
         $genre = $this->genre;
-    return "Rate: $this->rate, Year: $this->year, Genre: $genre->name";
+        return "$genre->name";
     }
 }
 class Genre
@@ -29,29 +29,79 @@ class Genre
     public $name;
     public function __construct(string $name)
     {
-    $this->name = $name;
+        $this->name = $name;
     }
 }
 
 
-   $spiderman = new Movie('spiderman no way home', new Genre('adventure'),2021,5,'https://picsum.photos/200/300');
-   $peterPan =  new Movie('Peter Pan & Wendy', new Genre('fantasy'), 2023,5,'https://picsum.photos/200/300');
-   $QueViva = new Movie('¡Que Viva México! ',new Genre('drama'), 2023,5,'https://picsum.photos/200/300');
- /*    new Movie('Parasite', 'thriller', 2019,5,'https://picsum.photos/200/300'),
-    new Movie('Supernatural ', 'sci-fi', 2005,5,'https://picsum.photos/200/300'),
-    new Movie('Kill Bill: Volume I', 'thriller', 2003,5,'https://picsum.photos/200/300'),
-    new Movie('Citadel', 'action', 2023,5,'https://picsum.photos/200/300'),
-    new Movie('Babylon', 'historical drama', 2022,5,'https://picsum.photos/200/300'),
-    new Movie('La chiave', 'drama', 1983,5,'https://picsum.photos/200/300'),
-    new Movie('Nerosubianco', 'drama', 1969,5,'https://picsum.photos/200/300'),
-    new Movie('Shutter Island', 'psycho thriller', 2010,5,'https://picsum.photos/200/300'),
-    new Movie('Brothers', 'drama', 2009,5,'https://picsum.photos/200/300'), */
-$movies =[
+$spiderman = new Movie('Spiderman no way home', new Genre('adventure'), 2021, 'https://picsum.photos/200/300',4);
+$peterPan =  new Movie('Peter Pan & Wendy', new Genre('fantasy'), 2023,  'https://picsum.photos/200/300',2);
+$QueViva = new Movie('¡Que Viva México! ', new Genre('drama'), 2023, 'https://picsum.photos/200/300',3);
+$parasite = new Movie('Parasite', new Genre('sci-fi'), 2019,'https://picsum.photos/200/300',5);
+/*     new Movie('Kill Bill: Volume I', 'thriller', 2003,'https://picsum.photos/200/300',5);
+    new Movie('Citadel', 'action', 2023,'https://picsum.photos/200/300',5);
+    new Movie('Babylon', 'historical drama', 2022,'https://picsum.photos/200/300',5);
+    new Movie('La chiave', 'drama', 1983,'https://picsum.photos/200/300',5);
+    new Movie('Nerosubianco', 'drama', 1969,'https://picsum.photos/200/300',5);
+    new Movie('Shutter Island', 'psycho thriller', 2010,'https://picsum.photos/200/300',5);
+    new Movie('Brothers', 'drama', 2009,'https://picsum.photos/200/300',5); */
+
+$movies = [
     $peterPan,
     $QueViva,
-    $spiderman
+    $spiderman,
+    $parasite,
 ];
 foreach ($movies as $movie) {
-var_dump($movie);
+    var_dump($movie);
 }
 ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <?php foreach ($movies as $movie) : ?>
+                    <div class="col-3">
+                        <div class="card">
+                            <img class="card-img-top" src="<?=$movie->imgPath;?>" alt="">
+                            <div class="card-body">
+                                <h4 class="card-title"><?=$movie->name;?></h4>
+                                <p class="card-text">Anno uscita: <?=$movie->year?></p>
+                                <p class="card-text">Voto: <?=$movie->rate?></p>
+                                <p class="card-text">Genre: <?=$movie->getGenre()?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
